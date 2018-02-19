3ec57ba6c854f71b70d941e922b60879c7797493
MichaelCoey <michaelcoey@outlook.com>
2018-02-19 11:45:27 +0000

    Antrim and Newtownabbey Council Menu - All Strings Connected and loading based on spinner selection.

--- Univeristy Work/Year 4/Semester 2/COM562 - Project/3. Application/LocalAmenitiesApplication/app/src/main/java/uk/ac/ulster/michaelcoey/localamenitiesapplication/AntrimCouncilInformation_Activity.java
+++ Univeristy Work/Year 4/Semester 2/COM562 - Project/3. Application/LocalAmenitiesApplication/app/src/main/java/uk/ac/ulster/michaelcoey/localamenitiesapplication/AntrimCouncilInformation_Activity.java
@@ -20,13 +20,10 @@ public class AntrimCouncilInformation_Activity extends AppCompatActivity impleme
         setContentView(R.layout.activity_antrim_council_information);
 
         spinner = (Spinner) findViewById(R.id.spn_AntrimCouncilInfo);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.ANCIM_Spinner_Array_Titles,
-                android.R.layout.simple_spinner_item);
-
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.ANCIM_Spinner_Array_Titles, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
-
     }
 
     @Override
@@ -45,6 +42,47 @@ public class AntrimCouncilInformation_Activity extends AppCompatActivity impleme
 
                 textElement = (TextView) findViewById(R.id.txtANCSAdditional);
                 textElement.setText(R.string.ANCIM_Spinner_additional_BirthsDeathsMarriages);
+                break;
+            case 1:
+                textElement = (TextView) findViewById(R.id.txtANCSDescription);
+                textElement.setText(R.string.ANCIM_Spinner_description_BuildingControl);
+
+                textElement = (TextView) findViewById(R.id.txtANCSContact);
+                textElement.setText(R.string.ANCIM_Spinner_contact_BuildingControl);
+
+                textElement = (TextView) findViewById(R.id.txtANCSAdditional);
+                textElement.setText(R.string.ANCIM_Spinner_additional_BuildingControl);
+                break;
+            case 2:
+                textElement = (TextView) findViewById(R.id.txtANCSDescription);
+                textElement.setText(R.string.ANCIM_Spinner_description_Cemeteries);
+
+                textElement = (TextView) findViewById(R.id.txtANCSContact);
+                textElement.setText(R.string.ANCIM_Spinner_contact_Cemeteries);
+
+                textElement = (TextView) findViewById(R.id.txtANCSAdditional);
+                textElement.setText(R.string.ANCIM_Spinner_additional_Cemeteries);
+                break;
+            case 3:
+                textElement = (TextView) findViewById(R.id.txtANCSDescription);
+                textElement.setText(R.string.ANCIM_Spinner_description_Dogs);
+
+                textElement = (TextView) findViewById(R.id.txtANCSContact);
+                textElement.setText(R.string.ANCIM_Spinner_contact_Dogs);
+
+                textElement = (TextView) findViewById(R.id.txtANCSAdditional);
+                textElement.setText(R.string.ANCIM_Spinner_additional_Dogs);
+                break;
+            case 4:
+                textElement = (TextView) findViewById(R.id.txtANCSDescription);
+                textElement.setText(R.string.ANCIM_Spinner_description_WasteRecycling);
+
+                textElement = (TextView) findViewById(R.id.txtANCSContact);
+                textElement.setText(R.string.ANCIM_Spinner_contact_WasteRecycling);
+
+                textElement = (TextView) findViewById(R.id.txtANCSAdditional);
+                textElement.setText(R.string.ANCIM_Spinner_additional_WasteRecycling);
+                break;
         }
     }
 
@@ -53,77 +91,4 @@ public class AntrimCouncilInformation_Activity extends AppCompatActivity impleme
     {
 
     }
-
-//    @Override
-//    public void onItemSelected(AdapterView<?> parent, View v, int position,
-//                               long id) {
-//        TextView textElement;
-//
-//        switch (position) {
-//            case 0:
-//                textElement = (TextView) findViewById(R.id.txtEducationDescription);
-//                textElement
-//                        .setText(R.string.Education_Spinner_description_PlayGroups);
-//
-//                textElement = (TextView) findViewById(R.id.txtEducationContact);
-//                textElement.setText(R.string.Education_Spinner_contact_PlayGroups);
-//
-//                textElement = (TextView) findViewById(R.id.txtEducationAdditional);
-//                textElement.setText(R.string.Education_Spinner_additional_PlayGroups);
-//
-//                break;
-//            case 1:
-//                textElement = (TextView) findViewById(R.id.txtEducationDescription);
-//                textElement
-//                        .setText(R.string.Education_Spinner_description_Primary);
-//
-//                textElement = (TextView) findViewById(R.id.txtEducationContact);
-//                textElement
-//                        .setText(R.string.Education_Spinner_contact_Primary);
-//
-//                textElement = (TextView) findViewById(R.id.txtEducationAdditional);
-//                textElement.setText(R.string.Education_Spinner_additional_Primary);
-//                break;
-//            case 2:
-//                textElement = (TextView) findViewById(R.id.txtEducationDescription);
-//                textElement
-//                        .setText(R.string.Education_Spinner_description_Secondary);
-//
-//                textElement = (TextView) findViewById(R.id.txtEducationContact);
-//                textElement
-//                        .setText(R.string.Education_Spinner_contact_Secondary);
-//
-//                textElement = (TextView) findViewById(R.id.txtEducationAdditional);
-//                textElement.setText(R.string.Education_Spinner_additional_Secondary);
-//                break;
-//            case 3:
-//                textElement = (TextView) findViewById(R.id.txtEducationDescription);
-//                textElement
-//                        .setText(R.string.Education_Spinner_description_HigherEducation);
-//                textElement = (TextView) findViewById(R.id.txtEducationContact);
-//                textElement
-//                        .setText(R.string.Education_Spinner_contact_HigherEducation);
-//
-//                textElement = (TextView) findViewById(R.id.txtEducationAdditional);
-//                textElement.setText(R.string.Education_Spinner_additional_HigherEducation);
-//                break;
-//            case 4:
-//                textElement = (TextView) findViewById(R.id.txtEducationDescription);
-//                textElement
-//                        .setText(R.string.Education_Spinner_description_ESQL);
-//
-//                textElement = (TextView) findViewById(R.id.txtEducationContact);
-//                textElement
-//                        .setText(R.string.Education_Spinner_contact_ESQL);
-//
-//                textElement = (TextView) findViewById(R.id.txtEducationAdditional);
-//                textElement
-//                        .setText(R.string.Education_Spinner_additional_ESQL);
-//
-//                break;
-//
-//        }
-//
-//    }
-
 }
